4727d51abe0ef50efc8f0c6a5e12e7c51a6608a7
Sam Poulis <sjpoulis@gmail.com>
2024-10-03 21:16:02 -0500

    Template for building chrome extensions with react

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p
+        style={{
+         color: "Green"
+        }}>
+          Hello! 
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Oh yeah
         </a>
       </header>
     </div>
